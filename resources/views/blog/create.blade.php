22889216c43cb7ae12847a9798b06743c0e32a8c
roleenboticario1 <roleenboticaro1@gmail.com>
2022-06-14 22:26:51 +0800

    first commit

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-9 mt-2">
             @if ($errors->any())
-                <div class="alert alert-danger">
+                <div class="alert alert-danger fade-out">
                     <strong>Whoops!</strong> There were some problems with your input.<br><br>
                     <ul>
                         @foreach ($errors->all() as $error)
@@ -19,11 +19,11 @@
            <form action="{{ url('store') }}" method="POST">
             @csrf
            <div class="form-group mt-4">
-            <input type="text" class="form-control padding0 transition-none background-color-transparent border1-solid-ffff border-buttom-color9e9e9e border-radius-none" name="title" placeholder="Enter Title">
+            <input type="text" class="form-control padding0 transition-none background-color-transparent border1-solid-none border-buttom-color9e9e9e border-radius-none" name="title" placeholder="Enter Title">
            <!--   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
            </div>
            <div class="form-group mt-4">
-            <textarea class="form-control padding0 transition-none background-color-transparent border1-solid-ffff border-buttom-color9e9e9e border-radius-none" name="body" placeholder="Enter Content" rows="3" style="resize: none"></textarea>
+            <textarea class="form-control padding0 transition-none background-color-transparent border1-solid-none border-buttom-color9e9e9e border-radius-none" name="body" placeholder="Enter Content" rows="3" style="resize: none"></textarea>
            </div>
            <div class="form-group mt-4">
            <button type="submit" class="btn btn background-color26a69a text-white border-radius0-1rem btn-sm padding-left-percent3 padding-right-percent3">POST</button>
--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-9 mt-2">
             @if ($errors->any())
-                <div class="alert alert-danger">
+                <div class="alert alert-danger fade-out">
                     <strong>Whoops!</strong> There were some problems with your input.<br><br>
                     <ul>
                         @foreach ($errors->all() as $error)
@@ -20,11 +20,11 @@
             @csrf
            <div class="form-group mt-4">
             <label class="text-size12 color9eb2cb mb-3">Enter New Title</label>
-            <input type="text" class="form-control transition-none background-color-transparent border1-solid-ffff border-buttom-color4caf50 padding0 border-radius-none" name="title" value="{{ $post->title }} - Edited" placeholder="{{ $post->title }} - Edited">
+            <input type="text" class="form-control transition-none background-color-transparent border1-solid-none border-buttom-color4caf50 padding0 border-radius-none" name="title" value="{{ $post->title }} - Edited" placeholder="{{ $post->title }} - Edited">
            </div>
            <div class="form-group mt-3">
             <label class="text-size12 color9eb2cb mb-3">Enter New Body</label>
-            <textarea class="form-control transition-none background-color-transparent border1-solid-ffff border-buttom-color4caf50 padding0 border-radius-none" name="body" placeholder="{{ $post->body }} - Edited" rows="3" style="resize: none !important">{{ old('body', $post->body ?? null) }}</textarea>
+            <textarea class="form-control transition-none background-color-transparent border1-solid-none border-buttom-color4caf50 padding0 border-radius-none" name="body" placeholder="{{ $post->body }} - Edited" rows="3" style="resize: none !important">{{ old('body', $post->body ?? null) }}</textarea>
            </div>
            <div class="form-group mt-5">
            <button type="submit" class="btn btn background-color26a69a text-white border-radius0-1rem btn-sm padding-left-percent3 padding-right-percent3">SAVE</button>
